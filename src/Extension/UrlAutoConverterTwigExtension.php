23b13128f067ebd03c4c9dfcdaf960895e359a9b
core23 <mail@core23.de>
2018-10-31 13:52:06 +0100

    Added typecasts

--- src/Extension/UrlAutoConverterTwigExtension.php
+++ src/Extension/UrlAutoConverterTwigExtension.php
@@ -46,13 +46,13 @@ final class UrlAutoConverterTwigExtension extends AbstractExtension
         }
 
         // Replace Links with http://
-        $ret = preg_replace("#(^|[\n ])([\w]+?://[\w\#$%&~/.\-;:=,?@\[\]+]*)#is", '\\1<a href="\\2"'.$attr.'>\\2</a>', $ret);
+        $ret = (string) preg_replace("#(^|[\n ])([\w]+?://[\w\#$%&~/.\-;:=,?@\[\]+]*)#is", '\\1<a href="\\2"'.$attr.'>\\2</a>', $ret);
 
         // Replace Links without http://
-        $ret = preg_replace("#(^|[\n ])((www|ftp)\.[\w\#$%&~/.\-;:=,?@\[\]+]*)#is", '\\1<a href="http://\\2"'.$attr.'>\\2</a>', $ret);
+        $ret = (string) preg_replace("#(^|[\n ])((www|ftp)\.[\w\#$%&~/.\-;:=,?@\[\]+]*)#is", '\\1<a href="http://\\2"'.$attr.'>\\2</a>', $ret);
 
         // Replace Email Addresses
-        $ret = preg_replace("#(^|[\n ])([a-z0-9&\-_.]+?)@([\w\-]+\.([\w\-\.]+\.)*[\w]+)#i", '\\1<a href="mailto:\\2@\\3"'.$attr.'>\\2@\\3</a>', $ret);
+        $ret = (string) preg_replace("#(^|[\n ])([a-z0-9&\-_.]+?)@([\w\-]+\.([\w\-\.]+\.)*[\w]+)#i", '\\1<a href="mailto:\\2@\\3"'.$attr.'>\\2@\\3</a>', $ret);
 
         return substr($ret, 1);
     }
