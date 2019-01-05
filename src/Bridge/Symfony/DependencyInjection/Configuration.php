e9751127a68b48c5f478cb039243770a8cd0a046
core23 <mail@core23.de>
2019-01-05 08:04:59 +0100

    Fixed CS

--- src/Bridge/Symfony/DependencyInjection/Configuration.php
+++ src/Bridge/Symfony/DependencyInjection/Configuration.php
@@ -25,7 +25,7 @@ final class Configuration implements ConfigurationInterface
         $treeBuilder = new TreeBuilder('core23_twig');
 
         // Keep compatibility with symfony/config < 4.2
-        if (!\method_exists($treeBuilder, 'getRootNode')) {
+        if (!method_exists($treeBuilder, 'getRootNode')) {
             $rootNode = $treeBuilder->root('core23_twig');
         } else {
             $rootNode = $treeBuilder->getRootNode();
