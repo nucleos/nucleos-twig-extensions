181e97a35054eb2f135ca7116368e340166f905f
core23 <mail@core23.de>
2019-03-18 22:14:05 +0100

    [BUILD] Added more tests

---
+++ tests/Bridge/Symfony/Bundle/Core23TwigBundleTest.php
@@ -0,0 +1,31 @@
+<?php
+
+/*
+ * (c) Christian Gripp <mail@core23.de>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Core23\Twig\Tests\Bridge\Symfony\Bundle;
+
+use Core23\Twig\Bridge\Symfony\Bundle\Core23TwigBundle;
+use PHPUnit\Framework\TestCase;
+use Symfony\Component\HttpKernel\Bundle\BundleInterface;
+
+class Core23TwigBundleTest extends TestCase
+{
+    public function testItIsInstantiable(): void
+    {
+        $bundle = new Core23TwigBundle();
+
+        $this->assertInstanceOf(BundleInterface::class, $bundle);
+    }
+
+    public function testGetPath(): void
+    {
+        $bundle = new Core23TwigBundle();
+
+        $this->assertStringEndsWith('Bridge/Symfony/Bundle', \dirname($bundle->getPath()));
+    }
+}
